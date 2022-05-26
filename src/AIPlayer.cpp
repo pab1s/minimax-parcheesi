2bd3675f9ce888fb3dca6be2ac459bcac0849e8e
Pablo Olivares <pablolivares1502@gmail.com>
2022-05-26 23:24:49 +0200

    Implementado poda Alfa-Beta

--- src/AIPlayer.cpp
+++ src/AIPlayer.cpp
@@ -1,34 +1,90 @@
-# include "AIPlayer.h"
-# include "Parchis.h"
+#include "AIPlayer.h"
+
+#include "Parchis.h"
 
 const double masinf = 9999999999.0, menosinf = -9999999999.0;
 const double gana = masinf - 1, pierde = menosinf + 1;
 const int num_pieces = 4;
 const int PROFUNDIDAD_MINIMAX = 4;  // Umbral maximo de profundidad para el metodo MiniMax
-const int PROFUNDIDAD_ALFABETA = 6; // Umbral maximo de profundidad para la poda Alfa_Beta
+const int PROFUNDIDAD_ALFABETA = 6;  // Umbral maximo de profundidad para la poda Alfa_Beta
 
-bool AIPlayer::move(){
+bool AIPlayer::move() {
     cout << "Realizo un movimiento automatico" << endl;
-    
+
     color c_piece;
     int id_piece;
     int dice;
     think(c_piece, id_piece, dice);
 
-    cout << "Movimiento elegido: " << str(c_piece) << " " << id_piece << " " << dice << endl;
+    cout << "Movimiento elegido: " << str(c_piece) << " " << id_piece << " "
+         << dice << endl;
 
     actual->movePiece(c_piece, id_piece, dice);
     return true;
 }
 
-void AIPlayer::think(color & c_piece, int & id_piece, int & dice) const{
+void AIPlayer::think(color &c_piece, int &id_piece, int &dice) const {
+    c_piece = actual->getCurrentColor();
+    vector<int> current_dices;
+    vector<int> current_pieces;
+    current_dices = actual->getAvailableDices(c_piece);
+    dice = current_dices[rand() % current_dices.size()];
+    current_pieces = actual->getAvailablePieces(c_piece, dice);
+    if (current_pieces.size() > 0) {
+        id_piece = current_pieces[rand() % current_pieces.size()];
+    } else {
+        id_piece = SKIP_TURN;
+    }
+
+    double valor;  // Almacena el valor con el que se etiqueta el estado tras el
+                   // proceso de busqueda.
+    double alpha = menosinf,
+           beta = masinf;  // Cotas iniciales de la poda AlfaBeta
+    // Llamada a la función para la poda (los parámetros son solo una
+    // sugerencia, se pueden modificar).
+    valor = Poda_AlfaBeta(*actual, jugador, 0, PROFUNDIDAD_ALFABETA, c_piece,
+                          id_piece, dice, alpha, beta, ValoracionTest);
+    cout << "Valor MiniMax: " << valor << "  Accion: " << str(c_piece) << " "
+         << id_piece << " " << dice << endl;
+
+    // ----------------------------------------------------------------- //
+
+    // Si quiero poder manejar varias heurísticas, puedo usar la variable id del
+    // agente para usar una u otra.
+    /*
+    switch (id) {
+        case 0:
+            valor = Poda_AlfaBeta(*actual, jugador, 0, PROFUNDIDAD_ALFABETA,
+                                  c_piece, id_piece, dice, alpha, beta,
+                                  ValoracionTest);
+            break;
+        case 1:
+            valor = Poda_AlfaBeta(*actual, jugador, 0, PROFUNDIDAD_ALFABETA,
+                                  c_piece, id_piece, dice, alpha, beta,
+                                  MiValoracion1);
+            break;
+        case 2:
+            valor = Poda_AlfaBeta(*actual, jugador, 0, PROFUNDIDAD_ALFABETA,
+                                  c_piece, id_piece, dice, alpha, beta,
+                                  MiValoracion2);
+            break;
+    }
+    
+    cout << "Valor MiniMax: " << valor << "  Accion: " << str(c_piece) << " "
+         << id_piece << " " << dice << endl;
+    */
+}
+
+void AIPlayer::thinkRandom(color &c_piece, int &id_piece, int &dice) const {
     // IMPLEMENTACIÓN INICIAL DEL AGENTE
     // Esta implementación realiza un movimiento aleatorio.
-    // Se proporciona como ejemplo, pero se debe cambiar por una que realice un movimiento inteligente 
-    //como lo que se muestran al final de la función.
-    
-    // OBJETIVO: Asignar a las variables c_piece, id_piece, dice (pasadas por referencia) los valores, 
-    //respectivamente, de:
+    // Se proporciona como ejemplo, pero se debe cambiar por una que realice un
+    // movimiento inteligente
+    // como lo que se muestran al final de la función.
+
+    // OBJETIVO: Asignar a las variables c_piece, id_piece, dice (pasadas por
+    // referencia) los valores,
+    // respectivamente, de:
     // - color de ficha a mover
     // - identificador de la ficha que se va a mover
     // - valor del dado con el que se va a mover la ficha.
@@ -38,7 +94,8 @@ void AIPlayer::think(color & c_piece, int & id_piece, int & dice) const{
 
     // Vector que almacenará los dados que se pueden usar para el movimiento
     vector<int> current_dices;
-    // Vector que almacenará los ids de las fichas que se pueden mover para el dado elegido.
+    // Vector que almacenará los ids de las fichas que se pueden mover para el
+    // dado elegido.
     vector<int> current_pieces;
 
     // Se obtiene el vector de dados que se pueden usar para el movimiento
@@ -50,63 +107,58 @@ void AIPlayer::think(color & c_piece, int & id_piece, int & dice) const{
     current_pieces = actual->getAvailablePieces(c_piece, dice);
 
     // Si tengo fichas para el dado elegido muevo una al azar.
-    if(current_pieces.size() > 0){
+    if (current_pieces.size() > 0) {
         id_piece = current_pieces[rand() % current_pieces.size()];
-    }
-    else{
-        // Si no tengo fichas para el dado elegido, pasa turno (la macro SKIP_TURN me permite no mover).
+    } else {
+        // Si no tengo fichas para el dado elegido, pasa turno (la macro
+        // SKIP_TURN me permite no mover).
         id_piece = SKIP_TURN;
     }
 
     /*
-    // El siguiente código se proporciona como sugerencia para iniciar la implementación del agente.
+    // El siguiente código se proporciona como sugerencia para iniciar la
+    implementación del agente.
 
-    double valor; // Almacena el valor con el que se etiqueta el estado tras el proceso de busqueda.
-    double alpha = menosinf, beta = masinf; // Cotas iniciales de la poda AlfaBeta
-    // Llamada a la función para la poda (los parámetros son solo una sugerencia, se pueden modificar).
-    valor = Poda_AlfaBeta(*actual, jugador, 0, PROFUNDIDAD_ALFABETA, c_piece, id_piece, dice, alpha, beta, ValoracionTest);
-    cout << "Valor MiniMax: " << valor << "  Accion: " << str(c_piece) << " " << id_piece << " " << dice << endl;
+    double valor; // Almacena el valor con el que se etiqueta el estado tras el
+    proceso de busqueda. double alpha = menosinf, beta = masinf; // Cotas
+    iniciales de la poda AlfaBeta
+    // Llamada a la función para la poda (los parámetros son solo una
+    sugerencia, se pueden modificar). valor = Poda_AlfaBeta(*actual, jugador, 0,
+    PROFUNDIDAD_ALFABETA, c_piece, id_piece, dice, alpha, beta, ValoracionTest);
+    cout << "Valor MiniMax: " << valor << "  Accion: " << str(c_piece) << " " <<
+    id_piece << " " << dice << endl;
 
     // ----------------------------------------------------------------- //
 
-    // Si quiero poder manejar varias heurísticas, puedo usar la variable id del agente para usar una u otra.
-    switch(id){
-        case 0:
-            valor = Poda_AlfaBeta(*actual, jugador, 0, PROFUNDIDAD_ALFABETA, c_piece, id_piece, dice, alpha, beta, ValoracionTest);
-            break;
-        case 1:
-            valor = Poda_AlfaBeta(*actual, jugador, 0, PROFUNDIDAD_ALFABETA, c_piece, id_piece, dice, alpha, beta, MiValoracion1);
-            break;
-        case 2:
-            valor = Poda_AlfaBeta(*actual, jugador, 0, PROFUNDIDAD_ALFABETA, c_piece, id_piece, dice, alpha, beta, MiValoracion2);
-            break;
+    // Si quiero poder manejar varias heurísticas, puedo usar la variable id del
+    agente para usar una u otra. switch(id){ case 0: valor =
+    Poda_AlfaBeta(*actual, jugador, 0, PROFUNDIDAD_ALFABETA, c_piece, id_piece,
+    dice, alpha, beta, ValoracionTest); break; case 1: valor =
+    Poda_AlfaBeta(*actual, jugador, 0, PROFUNDIDAD_ALFABETA, c_piece, id_piece,
+    dice, alpha, beta, MiValoracion1); break; case 2: valor =
+    Poda_AlfaBeta(*actual, jugador, 0, PROFUNDIDAD_ALFABETA, c_piece, id_piece,
+    dice, alpha, beta, MiValoracion2); break;
     }
-    cout << "Valor MiniMax: " << valor << "  Accion: " << str(c_piece) << " " << id_piece << " " << dice << endl;
+    cout << "Valor MiniMax: " << valor << "  Accion: " << str(c_piece) << " " <<
+    id_piece << " " << dice << endl;
 
     */
 }
 
-
-
-double AIPlayer::ValoracionTest(const Parchis &estado, int jugador)
-{
-    // Heurística de prueba proporcionada para validar el funcionamiento del algoritmo de búsqueda.
-
+double AIPlayer::ValoracionTest(const Parchis &estado, int jugador) {
+    // Heurística de prueba proporcionada para validar el funcionamiento del
+    // algoritmo de búsqueda.
 
     int ganador = estado.getWinner();
     int oponente = (jugador + 1) % 2;
 
-    // Si hay un ganador, devuelvo más/menos infinito, según si he ganado yo o el oponente.
-    if (ganador == jugador)
-    {
+    // Si hay un ganador, devuelvo más/menos infinito, según si he ganado yo o
+    // el oponente.
+    if (ganador == jugador) {
         return gana;
-    }
-    else if (ganador == oponente)
-    {
+    } else if (ganador == oponente) {
         return pierde;
-    }
-    else
-    {
+    } else {
         // Colores que juega mi jugador y colores del oponente
         vector<color> my_colors = estado.getPlayerColors(jugador);
         vector<color> op_colors = estado.getPlayerColors(oponente);
@@ -114,19 +166,15 @@ double AIPlayer::ValoracionTest(const Parchis &estado, int jugador)
         // Recorro todas las fichas de mi jugador
         int puntuacion_jugador = 0;
         // Recorro colores de mi jugador.
-        for (int i = 0; i < my_colors.size(); i++)
-        {
+        for (int i = 0; i < my_colors.size(); i++) {
             color c = my_colors[i];
             // Recorro las fichas de ese color.
-            for (int j = 0; j < num_pieces; j++)
-            {
-                // Valoro positivamente que la ficha esté en casilla segura o meta.
-                if (estado.isSafePiece(c, j))
-                {
+            for (int j = 0; j < num_pieces; j++) {
+                // Valoro positivamente que la ficha esté en casilla segura o
+                // meta.
+                if (estado.isSafePiece(c, j)) {
                     puntuacion_jugador++;
-                }
-                else if (estado.getBoard().getPiece(c, j).type == goal)
-                {
+                } else if (estado.getBoard().getPiece(c, j).type == goal) {
                     puntuacion_jugador += 5;
                 }
             }
@@ -135,26 +183,73 @@ double AIPlayer::ValoracionTest(const Parchis &estado, int jugador)
         // Recorro todas las fichas del oponente
         int puntuacion_oponente = 0;
         // Recorro colores del oponente.
-        for (int i = 0; i < op_colors.size(); i++)
-        {
+        for (int i = 0; i < op_colors.size(); i++) {
             color c = op_colors[i];
             // Recorro las fichas de ese color.
-            for (int j = 0; j < num_pieces; j++)
-            {
-                if (estado.isSafePiece(c, j))
-                {
-                    // Valoro negativamente que la ficha esté en casilla segura o meta.
+            for (int j = 0; j < num_pieces; j++) {
+                if (estado.isSafePiece(c, j)) {
+                    // Valoro negativamente que la ficha esté en casilla segura
+                    // o meta.
                     puntuacion_oponente++;
-                }
-                else if (estado.getBoard().getPiece(c, j).type == goal)
-                {
+                } else if (estado.getBoard().getPiece(c, j).type == goal) {
                     puntuacion_oponente += 5;
                 }
             }
         }
 
-        // Devuelvo la puntuación de mi jugador menos la puntuación del oponente.
+        // Devuelvo la puntuación de mi jugador menos la puntuación del
+        // oponente.
         return puntuacion_jugador - puntuacion_oponente;
     }
 }
 
+double AIPlayer::Poda_AlfaBeta(const Parchis &actual, int jugador, int profundidad, int profundidad_max, color &c_piece, int &id_piece, int &dice, double alpha, double beta, double (*heuristic)(const Parchis &, int)) const {
+    int last_dice = -1;
+    int last_id_piece = -1;
+    color last_c_piece = none;
+    int jugador_actual = actual.getCurrentPlayerId();
+    Parchis sucesor = actual.generateNextMoveDescending(last_c_piece, last_id_piece, last_dice);
+
+    if (profundidad == profundidad_max or actual.gameOver()) {
+        return heuristic(actual, jugador);
+    } else {
+        while (!(actual == sucesor)) {
+            if (jugador == jugador_actual) {
+                int prev_alpha = alpha;
+
+                alpha = max(alpha, Poda_AlfaBeta(sucesor, jugador, profundidad + 1, profundidad_max, last_c_piece, last_id_piece, last_dice, alpha, beta, heuristic));
+                
+                if (alpha != prev_alpha and profundidad == 0) {
+                    /*c_piece = get<0>(sucesor.getLastAction());
+                    id_piece = get<1>(sucesor.getLastAction());
+                    dice = get<2>(sucesor.getLastAction());*/
+                    c_piece = last_c_piece;
+                    id_piece = last_id_piece;
+                    dice = last_dice;
+                }
+
+                if (alpha >= beta) {
+                    //return beta;
+                    break;
+                }
+
+            } else {
+                beta = min(beta, Poda_AlfaBeta(sucesor, jugador, profundidad + 1, profundidad_max, last_c_piece, last_id_piece, last_dice, alpha, beta, heuristic));
+                
+                if (beta <= alpha) {
+                    //return alpha;
+                    break;
+                }
+            }
+            sucesor = actual.generateNextMoveDescending(last_c_piece, last_id_piece, last_dice);
+        }
+        
+        if (jugador == jugador_actual) {
+            return alpha;
+        } else {
+            return beta;
+        }
+    }
+
+   return EXIT_FAILURE;
+}
